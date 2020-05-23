ac9f9c7f1b0bd2be89bfca4882aa825cabcca300
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-23 13:48:26 -0300

    [ADD] Added Command class and Message class

---
+++ src/Common/Domain.Core/Commands/Command.cs
@@ -0,0 +1,20 @@
+﻿using EShop.Domain.Core.Messages;
+using FluentValidation.Results;
+using System;
+
+namespace EShop.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; private set; }
+
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public abstract bool IsValid();
+    }
+}
