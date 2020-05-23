e86a15897bdd17151e1e58d9253c6289dc25f13b
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-23 17:16:37 -0300

    [ADD] Added IBus interface

---
+++ src/Common/Domain.Core/Bus/IBus.cs
@@ -0,0 +1,10 @@
+﻿using EShop.Domain.Core.Commands;
+using System.Threading.Tasks;
+
+namespace EShop.Domain.Core.Bus
+{
+    public interface IBus
+    {
+        Task Send<TCommand>(TCommand command) where TCommand : Command;
+    }
+}
