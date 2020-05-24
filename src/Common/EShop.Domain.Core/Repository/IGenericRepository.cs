d5726c70092e55b0cddab07c37ef120cdf3671da
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-24 18:39:07 -0300

    [ADD] Added IGenericRepository interface

---
+++ src/Common/EShop.Domain.Core/Repository/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using EShop.Domain.Core.Entities;
+using System;
+using System.Linq;
+
+namespace EShop.Domain.Core.Repository
+{
+    public interface IGenericRepository<TEntity> where TEntity: Entity
+    {
+        IQueryable<TEntity> GetAll();
+        TEntity Get(Guid id);   
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(Guid id);      
+    }
+}
