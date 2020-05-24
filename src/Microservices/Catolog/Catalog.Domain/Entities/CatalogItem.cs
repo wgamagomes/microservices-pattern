0dbda67702893205ed01858e404c4b825db7e652
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-24 18:06:14 -0300

    [ALT] Implemented  CatalogItem class

--- src/Microservices/Catolog/Catalog.Domain/Entities/CatalogItem.cs
+++ src/Microservices/Catolog/Catalog.Domain/Entities/CatalogItem.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using EShop.Domain.Core.Entities;
 
 namespace EShop.Catalog.Domain.Entities
 {
-    public class CatalogItem: Entity
+    public class CatalogItem : Entity
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureFileName { get; set; }
+        public string PictureUri { get; set; }
+        public int CatalogTypeId { get; set; }
+        public int CatalogBrandId { get; set; }
+        public int AvailableStock { get; set; }
+        public int RestockThreshold { get; set; }
+        public int MaxStockThreshold { get; set; }
+        public bool OnReorder { get; set; }
+        public CatalogItem()
+        {
+
+        }
     }
 }
