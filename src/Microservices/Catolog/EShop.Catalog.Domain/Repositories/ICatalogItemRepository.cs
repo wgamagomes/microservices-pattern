d75aa4a0461ad96d1dfc099e007214edd842a9d4
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-24 18:44:40 -0300

    [ADD] Added ICatalogItemRepository interface

---
+++ src/Microservices/Catolog/EShop.Catalog.Domain/Repositories/ICatalogItemRepository.cs
@@ -0,0 +1,9 @@
+﻿using EShop.Catalog.Domain.Entities;
+using EShop.Domain.Core.Repository;
+
+namespace EShop.Catalog.Domain.Repositories
+{
+    public interface ICatalogItemRepository: IGenericRepository<CatalogItem>
+    {
+    }
+}
