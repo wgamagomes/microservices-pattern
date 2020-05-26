018e604eb657127f55217c63e5bbd17cce7a46bf
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-26 17:46:13 -0300

    [ADD] Added CatalogItemRepository class

---
+++ src/Microservices/Catolog/Catalog.Infrastructure.Data/Repositories/CatalogItemRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.Catalog.Infrastructure.Data.Repositories
+{
+    public class CatalogItemRepository
+    {
+    }
+}
