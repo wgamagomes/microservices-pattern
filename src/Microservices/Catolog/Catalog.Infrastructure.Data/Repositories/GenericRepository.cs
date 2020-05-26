4e60f3daa96ae39ff94537fbb41ae09f606b5815
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-26 17:50:19 -0300

    [ADD] Added GenericRepository clss

---
+++ src/Microservices/Catolog/Catalog.Infrastructure.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using EShop.Domain.Core.Entities;
+using EShop.Domain.Core.Repository;
+using System;
+using System.Linq;
+
+namespace EShop.Catalog.Infrastructure.Data.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : Entity
+    {
+        public GenericRepository()
+        {
+
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
