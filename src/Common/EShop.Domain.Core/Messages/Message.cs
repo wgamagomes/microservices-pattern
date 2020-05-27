807a240004d59fd308324a263a241ed2f2cc4613
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-27 19:03:16 -0300

    [ALT] Alter Message class

--- src/Common/EShop.Domain.Core/Messages/Message.cs
+++ src/Common/EShop.Domain.Core/Messages/Message.cs
@@ -6,11 +6,14 @@ namespace EShop.Domain.Core.Messages
     public abstract class Message : IRequest<bool>
     {
         public string MessageType { get; protected set; }
-        public Guid AggregateId { get; protected set; }
+        public Guid Id { get; protected set; }
+        public DateTime CreationDate { get; private set; }
 
         protected Message()
         {
             MessageType = GetType().Name;
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
         }
     }
 }
