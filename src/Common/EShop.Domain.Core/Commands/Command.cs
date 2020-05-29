4c4cf956410ca2891b207c8bfaa9f928992cce42
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-29 00:41:03 -0300

    [ALT ] Alter command

--- src/Common/EShop.Domain.Core/Commands/Command.cs
+++ src/Common/EShop.Domain.Core/Commands/Command.cs
@@ -1,19 +1,11 @@
 ﻿using EShop.Domain.Core.Messages;
 using FluentValidation.Results;
-using System;
 
 namespace EShop.Domain.Core.Commands
 {
     public abstract class Command : Message
     {
-        public DateTime Timestamp { get; private set; }
-
         public ValidationResult ValidationResult { get; set; }
-
-        protected Command()
-        {
-            Timestamp = DateTime.Now;
-        }
 
         public abstract bool IsValid();
     }
