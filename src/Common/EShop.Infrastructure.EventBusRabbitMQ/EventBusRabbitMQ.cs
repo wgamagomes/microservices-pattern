71c47e474e7ccd844c0a9a19f395895cd209fbff
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-29 00:44:57 -0300

    [ADD] Implemented methods to publish event async

--- src/Common/EShop.Infrastructure.EventBusRabbitMQ/EventBusRabbitMQ.cs
+++ src/Common/EShop.Infrastructure.EventBusRabbitMQ/EventBusRabbitMQ.cs
@@ -1,13 +1,70 @@
 ﻿using EShop.Domain.Core.EventBus;
 using EShop.Domain.Core.Events;
+using EShop.Infrastructure.EventBusRabbitMQ.Interfaces;
+using Newtonsoft.Json;
+using Polly;
+using Polly.Retry;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace EShop.Infrastructure.EventBusRabbitMQ
 {
     public class EventBusRabbitMQ : IEventBus
     {
-        public void Publish(Event @event)
+        private readonly IRabbitMQConnection _rabbitMQConnection;
+        private readonly int _retryCount;
+        const string BROKER_NAME = "event_bus";
+
+        public EventBusRabbitMQ(IRabbitMQConnection rabbitMQConnection, int retryCount = 5)
         {
-            throw new System.NotImplementedException();
+            _rabbitMQConnection = rabbitMQConnection;
+            _retryCount = retryCount;
+        }
+
+        public Task PublishAsync(Event @event)
+        {
+            return Task.Run(() =>
+            {
+                var policy = GetRetryPolicy(_retryCount, (ex, time) =>
+                {
+                    //Log the exception here when RabbitMQ channel could not publish message
+                });
+
+                using (var channel = _rabbitMQConnection.GetChannel())
+                {
+                    channel.ExchangeDeclare(exchange: BROKER_NAME, type: ExchangeType.Direct);
+
+                    policy.Execute(() => PublishEvent(@event, channel));
+                }
+            });
+        }
+
+        private void PublishEvent(Event @event, IModel channel)
+        {
+            var eventName = @event.GetType().Name;
+            var message = JsonConvert.SerializeObject(@event);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            var properties = channel.CreateBasicProperties();
+            properties.DeliveryMode = 2; 
+
+            channel.BasicPublish(
+                exchange: BROKER_NAME,
+                routingKey: eventName,
+                mandatory: true,
+                basicProperties: properties,
+                body: body);
+        }
+
+        private RetryPolicy GetRetryPolicy(int retryCount, Action<Exception, TimeSpan> onRetry)
+        {
+            return RetryPolicy.Handle<BrokerUnreachableException>()
+                .Or<SocketException>()
+                .WaitAndRetry(retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), onRetry);
         }
 
         public void Subscribe<TEvent>() where TEvent : Event
