c77fae1fb599143a7cbeb96c962cd8cc77424c6a
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-29 00:43:08 -0300

    [ALT] Altered from CreateModel to GetChannel

--- src/Common/EShop.Infrastructure.EventBusRabbitMQ/Interfaces/IRabbitMQConnection.cs
+++ src/Common/EShop.Infrastructure.EventBusRabbitMQ/Interfaces/IRabbitMQConnection.cs
@@ -5,7 +5,7 @@ namespace EShop.Infrastructure.EventBusRabbitMQ.Interfaces
     public interface IRabbitMQConnection
     {
         bool IsConnected { get; }
-        IModel CreateModel();
+        IModel GetChannel();
     }
 }
 
