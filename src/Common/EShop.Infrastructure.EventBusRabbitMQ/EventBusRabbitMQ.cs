fd813d5b9844e9d655ec6e2cf3450c674d829259
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-28 21:55:00 -0300

    [ADD] Added EventBusRabbitMQ class

---
+++ src/Common/EShop.Infrastructure.EventBusRabbitMQ/EventBusRabbitMQ.cs
@@ -0,0 +1,18 @@
+﻿using EShop.Domain.Core.EventBus;
+using EShop.Domain.Core.Events;
+
+namespace EShop.Infrastructure.EventBusRabbitMQ
+{
+    public class EventBusRabbitMQ : IEventBus
+    {
+        public void Publish(Event @event)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Subscribe<TEvent>() where TEvent : Event
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
