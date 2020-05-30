b9284865bf87f8b1aa8796825b49acbe974adb03
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-30 17:41:27 -0300

    [ALT]  Added RegisterEventBus method

--- src/Microservices/Catolog/EShop.EShop.Catalog.API/Startup.cs
+++ src/Microservices/Catolog/EShop.EShop.Catalog.API/Startup.cs
@@ -1,8 +1,12 @@
+using EShop.Domain.Core.EventBus;
+using EShop.Infrastructure.EventBusRabbitMQ;
+using EShop.Infrastructure.EventBusRabbitMQ.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
 
 namespace Catalog.API
 {
@@ -19,6 +23,38 @@ namespace Catalog.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            RegisterEventBus(services);
+        }
+
+        private void RegisterEventBus(IServiceCollection services)
+        {
+            services.AddSingleton<IRabbitMQConnection>(sp =>
+            {
+                var config = new ConfigurationBuilder()
+                        .AddJsonFile("appsettings.json")
+                        .Build();
+
+                var factory = new ConnectionFactory();                
+
+                config.GetSection("RabbitMqConnection").Bind(factory);
+
+                return new RabbitMQConnection(factory, 5);
+            });
+
+            services.AddSingleton<IEventBus, EventBusRabbitMQ>(sp =>
+            {
+                var rabbitMQConnection = sp.GetRequiredService<IRabbitMQConnection>();
+
+                var retryCount = 5;
+
+                if (!string.IsNullOrEmpty(Configuration["EventBusRetryCount"]))
+                {
+                    retryCount = int.Parse(Configuration["EventBusRetryCount"]);
+                }
+
+                return new EventBusRabbitMQ(rabbitMQConnection, retryCount);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
