a8c2dce673a9e68fc5012c6ac9d5fb47a63e7e89
Wellington G Gomes <wgamagomes@gmail.com>
2020-05-31 21:20:35 -0300

    [ADD] Added CatalogController class

---
+++ src/Microservices/Catolog/EShop.EShop.Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController
+    {
+
+
+    }
+}
