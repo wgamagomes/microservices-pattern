c4b16c42f74291cf7fd493adcf9179b9a2cff092
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-03 00:42:53 -0300

    [ADD] Added Builder class to Inheritance

---
+++ src/Common/EShop.Framework/FluentBuilder/Builder.cs
@@ -0,0 +1,24 @@
+﻿namespace EShop.Framework.FluentBuilder
+{
+    public class Builder<TBuilder, TEntity> : IBuilder<TEntity>
+        where TBuilder : IBuilder<TEntity>, new()
+        where TEntity : class, new()
+    {
+        protected readonly TEntity _entity;
+
+        public Builder()
+        {
+            _entity = new TEntity();
+        }
+
+        public TEntity Build()
+        {
+            return _entity;
+        }
+
+        public static TBuilder NewBuilder()
+        {
+            return new TBuilder();
+        }
+    }
+}
