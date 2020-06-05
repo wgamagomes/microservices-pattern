08c340285eca5c2d12cad62cedb31ad55f82e53e
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-04 23:19:04 -0300

    [ADD] Added TestServerBuilder class

---
+++ src/Microservices/Catolog/EShop.Catalog.FunctionalTests/Host/TestServer.cs
@@ -0,0 +1,31 @@
+﻿using EShop.Common.FluentBuilder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.IO;
+using System.Reflection;
+
+namespace EShop.Catalog.FunctionalTests.Host
+{
+    public  class TestServerBuilder
+    {
+        public  TestServer CreateServer<TStartup>()
+            where TStartup: class
+        {
+            var path = Assembly.GetAssembly(typeof(TestServerBuilder))
+              .Location;
+
+            var hostBuilder = new WebHostBuilder()
+                .UseContentRoot(Path.GetDirectoryName(path));
+                //.ConfigureAppConfiguration(cb =>
+                //{
+                //    cb.AddJsonFile("Services/Catalog/appsettings.json", optional: false)
+                //    .AddEnvironmentVariables();
+                //}).UseStartup<TStartup>();
+
+            var testServer = new TestServer(hostBuilder);
+
+
+            return testServer;
+        }
+    }
+}
