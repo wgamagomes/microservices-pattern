3ff0842e3c9b8cec0563d1094f93ebdb2ad496a0
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-04 23:57:17 -0300

    [ALT] Altered TestServerBuilder class

--- src/Microservices/Catolog/EShop.Catalog.FunctionalTests/Host/TestServer.cs
+++ src/Microservices/Catolog/EShop.Catalog.FunctionalTests/Host/TestServer.cs
@@ -1,29 +1,28 @@
-﻿using EShop.Common.FluentBuilder;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
 using System.IO;
 using System.Reflection;
 
 namespace EShop.Catalog.FunctionalTests.Host
 {
-    public  class TestServerBuilder
+    public class TestServerBuilder
     {
-        public  TestServer CreateServer<TStartup>()
-            where TStartup: class
+        public TestServer CreateServer<TStartup>()
+            where TStartup : class
         {
             var path = Assembly.GetAssembly(typeof(TestServerBuilder))
               .Location;
 
             var hostBuilder = new WebHostBuilder()
-                .UseContentRoot(Path.GetDirectoryName(path));
-                //.ConfigureAppConfiguration(cb =>
-                //{
-                //    cb.AddJsonFile("Services/Catalog/appsettings.json", optional: false)
-                //    .AddEnvironmentVariables();
-                //}).UseStartup<TStartup>();
+                .UseContentRoot(Path.GetDirectoryName(path))
+                .ConfigureAppConfiguration(cb =>
+                {
+                    cb.AddJsonFile("Services/Catalog/appsettings.json", optional: false)
+                    .AddEnvironmentVariables();
+                }).UseStartup<TStartup>();
 
             var testServer = new TestServer(hostBuilder);
-
 
             return testServer;
         }
