2de67ee041f6acca5bc4adff4b5ee29d9f9c0ad8
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-05 00:38:58 -0300

    [ALT] Altered TestServerBuilder

--- src/Microservices/Catolog/EShop.Catalog.FunctionalTests/Host/TestServer.cs
+++ src/Microservices/Catolog/EShop.Catalog.FunctionalTests/Host/TestServer.cs
@@ -7,23 +7,29 @@ using System.Reflection;
 
 namespace EShop.Catalog.FunctionalTests.Host
 {
-    public class TestServerBuilder : Builder<TestServerBuilder, TestServer>
+    public class TestServerBuilder 
     {
+        private IWebHostBuilder _hostBuilder;
+
+        public  TestServer Build()
+        {
+           return new TestServer(_hostBuilder);
+
+        }
+
         public TestServerBuilder CreateServer<TStartup>(string appsettings)
             where TStartup : class
         {
             var path = Assembly.GetAssembly(typeof(TStartup))
               .Location;
 
-            var hostBuilder = new WebHostBuilder()
-                .UseContentRoot(Path.GetDirectoryName(path))
-                .ConfigureAppConfiguration(cb =>
-                {
-                    cb.AddJsonFile(appsettings, optional: false)
-                    .AddEnvironmentVariables();
-                }).UseStartup<TStartup>();
-
-            _entity = new TestServer(hostBuilder);
+            _hostBuilder = new WebHostBuilder()
+               .UseContentRoot(Path.GetDirectoryName(path))
+               .ConfigureAppConfiguration(cb =>
+               {
+                   cb.AddJsonFile(appsettings, optional: false)
+                   .AddEnvironmentVariables();
+               }).UseStartup<TStartup>();
 
             return this;
         }
