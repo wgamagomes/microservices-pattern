257398d27edeaa41c0193c4032aec851981978b4
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-06 12:27:43 -0300

    [ADD] Added IContextSeed interface

---
+++ src/Microservices/Catolog/EShop.Catalog.Infrastructure.Data.EF/interfaces/IContextSeed.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+
+namespace EShop.Catalog.Infrastructure.Data.EF.interfaces
+{
+    public interface IContextSeed
+    {
+        Task SeedAsync<TOptions>(DbContext context, IOptions<TOptions> settings)
+             where TOptions : class, new();
+    }
+}
