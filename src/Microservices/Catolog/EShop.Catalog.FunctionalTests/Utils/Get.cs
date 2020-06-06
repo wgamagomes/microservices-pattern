6c24613db9e54263592ed5bdc72de7618eca8b58
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-06 18:54:12 -0300

    [ADD] Added Get util class

---
+++ src/Microservices/Catolog/EShop.Catalog.FunctionalTests/Utils/Get.cs
@@ -0,0 +1,15 @@
+﻿using EShop.Common.Web;
+
+
+namespace EShop.Catalog.FunctionalTests.Utils
+{
+    public static class Get
+    {
+        public static string Items(bool paginated = false, int pageIndex = 0, int pageSize = 10)
+        {
+            return paginated
+                ? $"api/catalog/items{Paginated.Get(pageIndex, pageSize)}"
+                : "api/catalog/items";
+        }
+    }
+}
