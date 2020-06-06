5846215b65523fb444ca51e898342aa1836a0004
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-06 12:27:17 -0300

    [ALT] Altered TestServerBuilder

--- src/Microservices/Catolog/EShop.Catalog.FunctionalTests/Host/TestServer.cs
+++ src/Microservices/Catolog/EShop.Catalog.FunctionalTests/Host/TestServer.cs
@@ -1,19 +1,23 @@
-﻿using EShop.Common.FluentBuilder;
+﻿using EShop.Catalog.Infrastructure.Data.EF.interfaces;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using System.IO;
 using System.Reflection;
 
 namespace EShop.Catalog.FunctionalTests.Host
 {
-    public class TestServerBuilder 
+    public class TestServerBuilder
     {
         private IWebHostBuilder _hostBuilder;
+        private TestServer _testServer;
 
-        public  TestServer Build()
+        public TestServer Build()
         {
-           return new TestServer(_hostBuilder);
+            return _testServer;
 
         }
 
@@ -30,6 +34,24 @@ namespace EShop.Catalog.FunctionalTests.Host
                    cb.AddJsonFile(appsettings, optional: false)
                    .AddEnvironmentVariables();
                }).UseStartup<TStartup>();
+
+            _testServer = new TestServer(_hostBuilder);
+            return this;
+        }
+
+        public TestServerBuilder WithMigrateDbContext<TContext, TOptions, TContextSeed>()
+            where TContext : DbContext
+            where TOptions : class, new()
+            where TContextSeed : IContextSeed, new()
+        {
+            _testServer
+                .Host
+                .MigrateDbContext<TContext>((context, services) =>
+                {
+                    var env = services.GetService<IWebHostEnvironment>();
+                    var settings = services.GetService<IOptions<TOptions>>();
+                    new TContextSeed().SeedAsync(context, settings);
+                });
 
             return this;
         }
