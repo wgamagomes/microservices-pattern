1cac991bf761fa820ff828355c31aac0ded3469f
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-06 18:53:01 -0300

    [ADD] Added Paginated class

---
+++ src/Common/EShop.Common/Web/Paginated.cs
@@ -0,0 +1,10 @@
+﻿namespace EShop.Common.Web
+{
+    public class Paginated
+    {
+        public static string Get(int pageIndex, int pageSize)
+        {
+            return $"?pageIndex={pageIndex}&pageSize={pageSize}";
+        }
+    }
+}
