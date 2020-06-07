d004705e58c42db1e3e210d1bb185ea711ca78df
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-07 14:20:36 -0300

    [ADD] Added EnvironmentVariableKeys and  EnvironmentVariableValues classes

---
+++ src/Common/EShop.Common/Configuration/EnvironmentVariable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EShop.Common.Configuration
+{
+    public static class EnvironmentVariableKeys
+    {
+        public const string MicroServiceName = "MICRO_SERVICE_NAME";
+        public const string PathBase = "PATH_BASE";
+        public const string Version = "VERSION";
+    }
+
+    public class EnvironmentVariableValues
+    {
+        public static  string MicroServiceName => Environment.GetEnvironmentVariable(EnvironmentVariableKeys.MicroServiceName) ?? "APP";
+        public static string PathBase => Environment.GetEnvironmentVariable(EnvironmentVariableKeys.PathBase) ?? string.Empty;
+        public static string Version => Environment.GetEnvironmentVariable(EnvironmentVariableKeys.Version) ?? "v1";
+    }
+}
