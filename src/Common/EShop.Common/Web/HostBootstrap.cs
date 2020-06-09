f77fae972c7686c4c4b8816e342606244144ed4b
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-08 21:55:30 -0300

    [ADD] Added HostBootstrap class

---
+++ src/Common/EShop.Common/Web/HostBootstrap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+namespace EShop.Common.Web
+{
+    public static class HostBootstrap
+    {
+        public static Task RunAsync<TStartup>()
+            where TStartup : class
+        {
+            return Host.CreateDefaultBuilder()
+               .ConfigureWebHostDefaults(webBuilder =>
+               {
+                   webBuilder
+                   .UseStartup<TStartup>();
+               })
+               .Build()
+               .RunAsync();
+        }
+    }
+}
