621b401d6eab2e78084424ae814bccf8a4ec1de6
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-08 21:57:46 -0300

    [ADD] Added HttpResponseExtension class

---
+++ src/Microservices/Catolog/EShop.Catalog.FunctionalTests/Extensions/HttpResponseExtension.cs
@@ -0,0 +1,17 @@
+﻿using EShop.Catalog.API.Model;
+using EShop.Catalog.API.ViewModel;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EShop.Catalog.FunctionalTests.Extensions
+{
+    public static class HttpResponseExtension
+    {
+        public static async Task<PaginatedViewModel<CatalogItem>> DeserializeCatalogAsync(this HttpResponseMessage response)
+        {
+            var items = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<PaginatedViewModel<CatalogItem>>(items);
+        }
+    }
+}
