1121050fc42f937197adf25ab702455d28fee732
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-10 21:39:38 -0300

    [ALT] Alter Entity class

--- src/Common/EShop.Domain.Core/Entities/Entity.cs
+++ src/Common/EShop.Domain.Core/Entities/Entity.cs
@@ -5,5 +5,8 @@ namespace EShop.Domain.Core.Entities
     public class Entity
     {
         public Guid Id { get; protected set; }
+        public DateTime InsertedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
     }
 }
