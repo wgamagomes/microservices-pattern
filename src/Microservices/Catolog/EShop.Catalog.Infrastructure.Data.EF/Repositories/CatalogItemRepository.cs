ef91e7a0be0e3636affc39d5862ca48ff67157ff
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-11 12:23:12 -0300

    [ALT] Alter CatalogItemRepository class

--- src/Microservices/Catolog/EShop.Catalog.Infrastructure.Data.EF/Repositories/CatalogItemRepository.cs
+++ src/Microservices/Catolog/EShop.Catalog.Infrastructure.Data.EF/Repositories/CatalogItemRepository.cs
@@ -1,9 +1,15 @@
 ﻿using EShop.Catalog.Domain.Entities;
 using EShop.Catalog.Domain.Repositories;
+using EShop.Catalog.Infrastructure.Data.Contexts;
 
 namespace EShop.Catalog.Infrastructure.Data.Repositories
 {
-    public class CatalogItemRepository: GenericRepository<CatalogItem>, ICatalogItemRepository
+    public class CatalogItemRepository : GenericRepository<CatalogItem>, ICatalogItemRepository
     {
+        public CatalogItemRepository(CatalogContext catalogContext)
+            : base(catalogContext)
+        {
+
+        }
     }
 }
