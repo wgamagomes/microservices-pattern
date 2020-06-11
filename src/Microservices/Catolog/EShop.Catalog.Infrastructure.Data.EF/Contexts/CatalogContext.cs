067e42d7f5b346d9badf4316a1efabcd2dcfa37b
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-11 12:22:45 -0300

    [ALT] Alter CatalogContext class

--- src/Microservices/Catolog/EShop.Catalog.Infrastructure.Data.EF/Contexts/CatalogContext.cs
+++ src/Microservices/Catolog/EShop.Catalog.Infrastructure.Data.EF/Contexts/CatalogContext.cs
@@ -1,8 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace EShop.Catalog.Infrastructure.Data.Contexts
 {
-    public class CatalogContext: DbContext
+    public class CatalogContext : DbContext
     {
+        public CatalogContext(DbContextOptions<CatalogContext> options)
+            : base(options)
+        {
+        }
     }
+
+
+
+
+
+
+
+
+
+//    public class CatalogContextFactory : IDesignTimeDbContextFactory<CatalogContext>
+//    {
+//        public CatalogContext CreateDbContext(string[] args)
+//        {
+//            var optionsBuilder = new DbContextOptionsBuilder<CatalogContext>();
+
+//            optionsBuilder.UseSqlServer("");
+
+//            return new CatalogContext(optionsBuilder.Options);
+//        }
+//    }
 }
