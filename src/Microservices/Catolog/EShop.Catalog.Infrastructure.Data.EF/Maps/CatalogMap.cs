5ade82dfa51cd18d9b280be93d0080e4715f4927
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-12 22:31:22 -0300

    [ADD] Added CatalogMap class

---
+++ src/Microservices/Catolog/EShop.Catalog.Infrastructure.Data.EF/Maps/CatalogMap.cs
@@ -0,0 +1,13 @@
+﻿using EShop.Catalog.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EShop.Catalog.Infrastructure.Data.EF.Maps
+{
+    public class CatalogMap : EntityTypeConfiguration<CatalogItem>
+    {
+        protected override void OnEntityBuild(EntityTypeBuilder<CatalogItem> builder)
+        {
+
+        }
+    }
+}
