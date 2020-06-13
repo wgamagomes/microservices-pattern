30fc2bf46a0bdce2666ea82ee99ae34918d9dc81
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-12 22:33:10 -0300

    [ADD] Added EntityTypeConfiguration class

---
+++ src/Microservices/Catolog/EShop.Catalog.Infrastructure.Data.EF/Maps/EntityTypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using EShop.Domain.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EShop.Catalog.Infrastructure.Data.EF.Maps
+{
+    public abstract class EntityTypeConfiguration<TEntity> : IEntityTypeConfiguration<TEntity>
+        where TEntity : Entity
+    {
+        public void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            //Configure here the generic map
+            OnEntityBuild(builder);
+        }
+
+        protected abstract void OnEntityBuild(EntityTypeBuilder<TEntity> builder);
+    }
+}
