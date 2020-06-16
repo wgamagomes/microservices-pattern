31994fb5bec37f78e572111b5587833dcb844e4a
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-16 13:59:38 -0300

    [ALT] Alter EntityTypeConfiguration class

--- src/Microservices/Catolog/EShop.Catalog.Infrastructure.Data.EF/EntityConfigurations/EntityTypeConfiguration.cs
+++ src/Microservices/Catolog/EShop.Catalog.Infrastructure.Data.EF/EntityConfigurations/EntityTypeConfiguration.cs
@@ -7,13 +7,9 @@ namespace EShop.Catalog.Infrastructure.Data.EF.EntityConfigurations
     public abstract class EntityTypeConfiguration<TEntity> : IEntityTypeConfiguration<TEntity>
         where TEntity : Entity
     {
-        public void Configure(EntityTypeBuilder<TEntity> builder)
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
         {
             builder.HasKey(e => e.Id);
-
-            OnEntityBuild(builder);
         }
-
-        protected abstract void OnEntityBuild(EntityTypeBuilder<TEntity> builder);
     }
 }
