ed106b5566ba4c56abd8f4252e0e915aca3f813c
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-16 13:59:02 -0300

    [ADD] Added CatalogBrandConfiguration class

---
+++ src/Microservices/Catolog/EShop.Catalog.Infrastructure.Data.EF/EntityConfigurations/CatalogBrandConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using EShop.Catalog.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EShop.Catalog.Infrastructure.Data.EF.EntityConfigurations
+{
+    public class CatalogBrandConfiguration : EntityTypeConfiguration<CatalogBrand>
+    {
+        public override void Configure(EntityTypeBuilder<CatalogBrand> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(cb => cb.Brand)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
