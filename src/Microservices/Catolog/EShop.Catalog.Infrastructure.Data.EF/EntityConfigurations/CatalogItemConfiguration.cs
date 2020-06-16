cdc65f2d75084e8300e347b4eed80582faa8b261
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-15 23:06:55 -0300

    [ALT] Altered from CatalogMap to CatalogItemConfiguration

---
+++ src/Microservices/Catolog/EShop.Catalog.Infrastructure.Data.EF/EntityConfigurations/CatalogItemConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using EShop.Catalog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EShop.Catalog.Infrastructure.Data.EF.Maps
+{
+    public class CatalogItemConfiguration : EntityTypeConfiguration<CatalogItem>
+    {
+        protected override void OnEntityBuild(EntityTypeBuilder<CatalogItem> builder)
+        {
+            builder.ToTable("Catalog");
+
+            builder.Property(ci => ci.Name)
+                .IsRequired(true)
+                .HasMaxLength(50);
+
+            builder.Property(ci => ci.Price)
+                .IsRequired(true);
+
+            builder.Property(ci => ci.PictureFileName)
+                .IsRequired(false);
+
+            builder.Ignore(ci => ci.PictureUri);
+
+            //builder.HasOne(ci => ci.CatalogBrand)
+            //    .WithMany()
+            //    .HasForeignKey(ci => ci.CatalogBrandId);
+
+            //builder.HasOne(ci => ci.CatalogType)
+            //    .WithMany()
+            //    .HasForeignKey(ci => ci.CatalogTypeId);
+        }
+    }
+}
