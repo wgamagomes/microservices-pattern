c63d5068d2abb03a4cf079d77bacb79a39b6cc18
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-15 23:05:10 -0300

    [ADD} Added CatalogTypeConfiguration class

---
+++ src/Microservices/Catolog/EShop.Catalog.Infrastructure.Data.EF/EntityConfigurations/CatalogTypeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using EShop.Catalog.Domain.Entities;
+using EShop.Catalog.Infrastructure.Data.EF.Maps;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EShop.Catalog.Infrastructure.Data.EF.EntityConfigurations
+{
+    public class CatalogTypeConfiguration : EntityTypeConfiguration<CatalogType>
+    {
+        protected override void OnEntityBuild(EntityTypeBuilder<CatalogType> builder)
+        {
+            builder.ToTable("CatalogType");
+
+            builder.Property(e => e.Type)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
