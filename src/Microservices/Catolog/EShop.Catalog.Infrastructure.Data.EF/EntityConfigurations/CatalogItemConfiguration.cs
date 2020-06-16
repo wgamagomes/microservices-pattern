6d066732641ce65412b8cef7d77908cf50a08827
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-16 14:00:23 -0300

    [ALT] Alter CatalogItemConfiguration class

--- src/Microservices/Catolog/EShop.Catalog.Infrastructure.Data.EF/EntityConfigurations/CatalogItemConfiguration.cs
+++ src/Microservices/Catolog/EShop.Catalog.Infrastructure.Data.EF/EntityConfigurations/CatalogItemConfiguration.cs
@@ -6,15 +6,16 @@ namespace EShop.Catalog.Infrastructure.Data.EF.EntityConfigurations
 {
     public class CatalogItemConfiguration : EntityTypeConfiguration<CatalogItem>
     {
-        protected override void OnEntityBuild(EntityTypeBuilder<CatalogItem> builder)
+        public override void Configure(EntityTypeBuilder<CatalogItem> builder)
         {
-            builder.ToTable("Catalog");
+            base.Configure(builder);
 
             builder.Property(ci => ci.Name)
                 .IsRequired(true)
                 .HasMaxLength(50);
 
             builder.Property(ci => ci.Price)
+                .HasColumnType("decimal(5,2)")
                 .IsRequired(true);
 
             builder.Property(ci => ci.PictureFileName)
@@ -22,9 +23,9 @@ namespace EShop.Catalog.Infrastructure.Data.EF.EntityConfigurations
 
             builder.Ignore(ci => ci.PictureUri);
 
-            //builder.HasOne(ci => ci.CatalogBrand)
-            //    .WithMany()
-            //    .HasForeignKey(ci => ci.CatalogBrandId);
+            builder.HasOne(ci => ci.CatalogBrand)
+                .WithMany()
+                .HasForeignKey(ci => ci.CatalogBrandId);
 
             builder.HasOne(ci => ci.CatalogType)
                 .WithMany()
