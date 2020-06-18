0b7d4607227ecaefb58de1eab7f51ee000955707
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-17 22:27:39 -0300

    [ADD] Added migration

---
+++ src/Microservices/Catolog/EShop.Catalog.Infrastructure.Data.EF/CatalogMigrations/20200616025812_Initial.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EShop.Catalog.Infrastructure.Data.EF.CatalogMigrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CatalogBrand",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    InsertedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    DeletedAt = table.Column<DateTime>(nullable: true),
+                    Brand = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogBrand", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CatalogType",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    InsertedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    DeletedAt = table.Column<DateTime>(nullable: true),
+                    Type = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CatalogItem",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    InsertedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    DeletedAt = table.Column<DateTime>(nullable: true),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(5,2)", nullable: false),
+                    PictureFileName = table.Column<string>(nullable: true),
+                    CatalogTypeId = table.Column<Guid>(nullable: false),
+                    CatalogBrandId = table.Column<Guid>(nullable: false),
+                    AvailableStock = table.Column<int>(nullable: false),
+                    RestockThreshold = table.Column<int>(nullable: false),
+                    MaxStockThreshold = table.Column<int>(nullable: false),
+                    OnReorder = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CatalogItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CatalogItem_CatalogBrand_CatalogBrandId",
+                        column: x => x.CatalogBrandId,
+                        principalTable: "CatalogBrand",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CatalogItem_CatalogType_CatalogTypeId",
+                        column: x => x.CatalogTypeId,
+                        principalTable: "CatalogType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CatalogItem_CatalogBrandId",
+                table: "CatalogItem",
+                column: "CatalogBrandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CatalogItem_CatalogTypeId",
+                table: "CatalogItem",
+                column: "CatalogTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CatalogItem");
+
+            migrationBuilder.DropTable(
+                name: "CatalogBrand");
+
+            migrationBuilder.DropTable(
+                name: "CatalogType");
+        }
+    }
+}
