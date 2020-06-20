e156d8fcb2e15e9b60d8851e6969fd1d13decae2
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-20 17:23:34 -0300

    [ADD] Added CatalogItemsQueryHandler class

---
+++ src/Microservices/Catolog/EShop.Catalog.Domain/QueryHandlers/CatalogItemsQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using EShop.Catalog.Domain.Dto;
+using EShop.Catalog.Domain.Query;
+using EShop.Common.Web;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EShop.Catalog.Domain.QueryHandlers
+{
+    public class CatalogItemsQueryHandler : IRequestHandler<CatalogItemsQuery, PaginatedResult<CatalogItemDto>>
+    {
+        public Task<PaginatedResult<CatalogItemDto>> Handle(CatalogItemsQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
