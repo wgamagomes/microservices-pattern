bc6db853afa4fb8932305f58a2848ff407fb2c79
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-20 17:22:15 -0300

    [ADD] Added CatalogItemDto class

--- src/Microservices/Catolog/EShop.Catalog.Domain/Dto/CatalogItemDto.cs
+++ src/Microservices/Catolog/EShop.Catalog.Domain/Dto/CatalogItemDto.cs
@@ -1,8 +1,16 @@
 ﻿using EShop.Common.Web.Interfaces;
+using System;
 
 namespace EShop.Catalog.Domain.Dto
 {
-    public class CatalogItemDto: IDto
+    public class CatalogItemDto : IDto
     {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
     }
 }
