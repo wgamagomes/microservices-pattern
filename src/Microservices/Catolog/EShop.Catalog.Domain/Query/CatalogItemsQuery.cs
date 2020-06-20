389a9dee7a464e80903fecd4f406fa6888919671
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-20 17:22:57 -0300

    [ADD] Added CatalogItemsQuery class

---
+++ src/Microservices/Catolog/EShop.Catalog.Domain/Query/CatalogItemsQuery.cs
@@ -0,0 +1,12 @@
+﻿using EShop.Catalog.Domain.Dto;
+using EShop.Common.Web;
+using MediatR;
+
+namespace EShop.Catalog.Domain.Query
+{
+    public class CatalogItemsQuery : IRequest<PaginatedResult<CatalogItemDto>>
+    {
+        public Paginated Paginated { get; set; }
+        public string Ids { get; set; }
+    }
+}
