88b84a682d89e27dda6cc5dbee81d30a5fb2dfe7
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-23 23:12:03 -0300

    [ALT] Alter CatalogItemsQuery class

--- src/Microservices/Catolog/EShop.Catalog.Domain/Query/CatalogItemsQuery.cs
+++ src/Microservices/Catolog/EShop.Catalog.Domain/Query/CatalogItemsQuery.cs
@@ -1,12 +1,12 @@
 ﻿using EShop.Catalog.Domain.Dto;
 using EShop.Common.Web;
 using MediatR;
+using System.Collections.Generic;
 
 namespace EShop.Catalog.Domain.Query
 {
-    public class CatalogItemsQuery : IRequest<PaginatedResult<CatalogItemDto>>
+    public class CatalogItemsQuery : IRequest<IEnumerable<CatalogItemDto>>
     {
-        public Paginated Paginated { get; set; }
-        public string Ids { get; set; }
+        public IEnumerable<string> Ids { get; set; }
     }
 }
