1f7c8a6292bba57a3e521a84e30046dec5d5cae5
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-23 23:34:27 -0300

    [ALT] Implemented Paginated Handle

--- src/Microservices/Catolog/EShop.Catalog.Domain/QueryHandlers/CatalogItemsQueryHandler.cs
+++ src/Microservices/Catolog/EShop.Catalog.Domain/QueryHandlers/CatalogItemsQueryHandler.cs
@@ -1,9 +1,12 @@
 ﻿using EShop.Catalog.Domain.Dto;
 using EShop.Catalog.Domain.Query;
+using EShop.Catalog.Domain.Repositories;
 using EShop.Common.Web;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,9 +14,25 @@ namespace EShop.Catalog.Domain.QueryHandlers
 {
     public class CatalogItemsQueryHandler : IRequestHandler<CatalogItemsPaginatedQuery, PaginatedResult<CatalogItemDto>>, IRequestHandler<CatalogItemsQuery, IEnumerable<CatalogItemDto>>
     {
+        private readonly ICatalogItemRepository _catalogItemRepository;
+
+        public CatalogItemsQueryHandler(ICatalogItemRepository catalogItemRepository)
+        {
+            _catalogItemRepository = catalogItemRepository;
+        }
         public async Task<PaginatedResult<CatalogItemDto>> Handle(CatalogItemsPaginatedQuery request, CancellationToken cancellationToken)
         {
-            return await Task.Run(() => new PaginatedResult<CatalogItemDto>(0, 0, 0, null));
+            var totalItems = await _catalogItemRepository.GetAll()
+                .LongCountAsync();
+
+            var itemsOnPage = await _catalogItemRepository.GetAll()
+                .Select(c => new CatalogItemDto(c))
+                .OrderBy(c => c.Name)
+                .Skip(request.Paginated.PageSize * request.Paginated.PageIndex)
+                .Take(request.Paginated.PageSize)
+                .ToListAsync();
+
+            return new PaginatedResult<CatalogItemDto>(request.Paginated.PageIndex, request.Paginated.PageSize, totalItems, itemsOnPage);
         }
 
         public async Task<IEnumerable<CatalogItemDto>> Handle(CatalogItemsQuery request, CancellationToken cancellationToken)
