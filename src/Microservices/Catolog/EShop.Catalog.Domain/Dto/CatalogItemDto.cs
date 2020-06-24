0a7ec5bb51a2cc6a2c768a6a25ea31e66f740200
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-23 23:35:24 -0300

    [ADD] Added properties and constructor

--- src/Microservices/Catolog/EShop.Catalog.Domain/Dto/CatalogItemDto.cs
+++ src/Microservices/Catolog/EShop.Catalog.Domain/Dto/CatalogItemDto.cs
@@ -1,4 +1,5 @@
-﻿using EShop.Common.Web.Interfaces;
+﻿using EShop.Catalog.Domain.Entities;
+using EShop.Common.Web.Interfaces;
 using System;
 
 namespace EShop.Catalog.Domain.Dto
@@ -6,11 +7,32 @@ namespace EShop.Catalog.Domain.Dto
     public class CatalogItemDto : IDto
     {
         public Guid Id { get; set; }
-
         public string Name { get; set; }
-
         public string Description { get; set; }
-
         public decimal Price { get; set; }
+        public string PictureFileName { get; set; }
+        public string PictureUri { get; set; }
+        public Guid CatalogTypeId { get; set; }
+        public Guid CatalogBrandId { get; set; }
+        public int AvailableStock { get; set; }
+        public int RestockThreshold { get; set; }
+        public int MaxStockThreshold { get; set; }
+        public bool OnReorder { get; set; }
+
+        public CatalogItemDto(CatalogItem catalogItem)
+        {
+            Id = catalogItem.Id;
+            Name = catalogItem.Name;
+            Description = catalogItem.Description;
+            Price = catalogItem.Price;
+            PictureFileName = catalogItem.PictureFileName;
+            PictureUri = catalogItem.PictureUri;
+            CatalogTypeId = catalogItem.CatalogTypeId;
+            CatalogBrandId = catalogItem.CatalogBrandId;
+            AvailableStock = catalogItem.AvailableStock;
+            RestockThreshold = catalogItem.RestockThreshold;
+            MaxStockThreshold = catalogItem.MaxStockThreshold;
+            OnReorder = catalogItem.OnReorder;
+        }
     }
 }
