783cfd134428d84328cf0ed6f64021a17638d171
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-24 20:33:09 -0300

    [ADD] Added validation

--- src/Microservices/Catolog/EShop.Catalog.Domain/Query/CatalogItemsQuery.cs
+++ src/Microservices/Catolog/EShop.Catalog.Domain/Query/CatalogItemsQuery.cs
@@ -1,12 +1,19 @@
 ﻿using EShop.Catalog.Domain.Dto;
-using EShop.Common.Web;
 using MediatR;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace EShop.Catalog.Domain.Query
 {
     public class CatalogItemsQuery : IRequest<IEnumerable<CatalogItemDto>>
     {
         public IEnumerable<string> Ids { get; set; }
+
+        public bool IsValid()
+        {
+            var guids = Ids.Select(id => (Ok: int.TryParse(id, out int x), Value: x));
+
+            return (!guids.All(nid => nid.Ok));           
+        }
     }
 }
