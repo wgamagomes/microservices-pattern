367bbd2d2c06de4c2d1f5b7ea6335a866aa36493
Wellington G Gomes <wgamagomes@gmail.com>
2020-06-26 21:38:53 -0800

    [ADD] Added Query class

---
+++ src/Common/EShop.Domain.Core/Queries/Query.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation.Results;
+using MediatR;
+
+namespace EShop.Domain.Core.Queries
+{
+    public abstract class Query<TResponse> : IRequest<TResponse>
+    {
+        public ValidationResult ValidationResult { get; set; }
+
+        public abstract bool IsValid();
+    }
+}
